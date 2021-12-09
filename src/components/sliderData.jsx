a93abeb45add76fd2994df9b39654565485102ec
Davi Pereira <davipereira@DTECM136583036.acad.unifor.br>
2021-12-09 16:33:24 -0300

    slider

---
+++ src/components/sliderData.jsx
@@ -0,0 +1,16 @@
+const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1633113089631-6456cccaadad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1639057073293-aae9a90e5db6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1639050154816-bced01e66bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1639057073293-aae9a90e5db6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60'
+    }
+]
+
+export default SliderData;
